2a8a9e03b8604de0bb9fe4ac91fdc1b333468c65
KongDeLiang <13674059697@163.com>
2020-07-08 16:30:30 +0800

    0708修改脚本

--- scripts/rbRunMoudle.sh
+++ scripts/rbRunMoudle.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-  roslaunch hsr_bringup co605_fight_go.launch
-wait
+  roslaunch hsr_bringup co605_fight_go.launch &
+  sleep 6
+  rosservice call /clear_robot_fault "{}" 
+  sleep 1
+  rosservice call /set_robot_enable "enable: true" 
 exit 0
